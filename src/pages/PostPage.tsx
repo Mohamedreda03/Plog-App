b68dffca0dc60dd5bc044b7a5d8c964b8adf65d7
Mohamedreda16042003 <mohamedreda16042003@gmail.com>
2023-09-21 22:04:54 +0300

    gfjdskal

--- src/pages/PostPage.tsx
+++ src/pages/PostPage.tsx
@@ -1,4 +1,4 @@
-import { Link, useParams } from "react-router-dom";
+import { Link, Navigate, useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { format } from "date-fns";
@@ -9,6 +9,7 @@ const PostPage = () => {
     const [post, setPost]: any = useState(null);
     const [user, setUser]: any = useState("");
     const [isLoading, setIsLoading]: any = useState(true);
+    const [Nav, setNav]: any = useState(false);
     const token: any = window.localStorage.getItem("token");
 
     useEffect(() => {
@@ -62,6 +63,9 @@ const PostPage = () => {
         );
     }
 
+    if (Nav) {
+        return <Navigate to={"/"} />;
+    }
     return (
         <div className="pt-[20px] pb-[40px]">
             {post.auther.userName === user.user && (
@@ -86,9 +90,11 @@ const PostPage = () => {
                         </svg>
                         Edit Post
                     </Link>
-                    <Link
-                        to={"/"}
-                        onClick={handleDelete}
+                    <div
+                        onClick={() => {
+                            handleDelete();
+                            setNav(true);
+                        }}
                         className="flex items-center gap-2 bg-red-500 rounded-lg px-[20px] hover:bg-red-600 hover:scale-[1.015] transition duration-150 p-[15px] text-white"
                     >
                         <svg
@@ -105,7 +111,7 @@ const PostPage = () => {
                                 d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
                             />
                         </svg>
-                    </Link>
+                    </div>
                 </div>
             )}
 
